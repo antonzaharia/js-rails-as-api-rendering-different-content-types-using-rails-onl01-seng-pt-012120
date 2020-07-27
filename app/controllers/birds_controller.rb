280954184e851ff7d48847abe53d2d5ab0d38058
ANTON ZAHARIA <zaharia_anton@yahoo.com>
2020-07-27 18:27:02 +0000

    Automatically backed up by Learn

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -4,4 +4,3 @@ class BirdsController < ApplicationController
     render plain: "Hello, #{@birds[3].name}"
   end
 end
-fetch('http://165.227.141.103:56630/birds').then(response => response.text()).then(text => console.log(text))
